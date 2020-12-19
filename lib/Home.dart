73950d7a2ad4a3d518df40410cac4c9dd7f959f9
polakamal <polakamal82@gmail.com>
2020-12-19 03:00:24 +0200

    uploading the app

---
+++ lib/Home.dart
@@ -0,0 +1,122 @@
+import 'dart:math';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:flutter/material.dart';
+import 'emoji.dart';
+emoji emojis = emoji();
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final Map<String,bool>Score= {};
+  final Map<String,String>choices = emojis.get_emoji();
+  int index =0;
+   int urscore=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(urscore.toString()+ '/' +choices.length.toString()),
+      ),
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      children: [
+       Column(
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
+         crossAxisAlignment: CrossAxisAlignment.end,
+         children: choices.keys.map((element) {
+           return Expanded(
+             child: Draggable<String>(
+               data: element,
+               child: Movable(Score[element] ==true ? '✔️': element ),
+               feedback: Movable(element),
+               childWhenDragging: null,
+
+             ),
+           );
+         }).toList(),
+       ),
+        Column(
+       mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: choices.keys.map((element){
+            return buildTarget(element);
+
+          }).toList()..shuffle(Random(index)),
+        ),
+      ],
+    ),
+    );
+  }
+  Widget buildTarget(element){
+   return DragTarget<String>(
+   builder: (context ,incoming,rejected)
+   {
+     if(Score[element]==true)
+     {
+
+       return Container(
+         color: Colors.white,
+         child: Text('اجابة صحيحة'),
+         alignment: Alignment.center,
+         height: 50,
+         width: 200,
+       );
+     }else
+       {
+      return Container(
+        color: Colors.white,
+       child: Text(choices[element]) ,
+        alignment: Alignment.center,
+        height: 50,
+        width: 200,);
+       }
+
+   },
+     onWillAccept: (data)=> data== element ,
+     onAccept: (data)
+     { setState(() {
+       if(emojis.is_finished()==true)
+       {
+         Alert(
+           context: context,
+           title: 'تهانينا!',
+           desc: 'لقد انتهيت من الاختبار',
+         ).show();
+
+         Score.clear();
+         urscore=0;
+        index++;
+       }else
+         {
+           emojis.next_Question();
+           Score[element]= true;
+           urscore++;
+         }
+
+     });
+
+
+     } ,
+     onLeave:(data) {},
+   );
+
+  }
+}
+class Movable extends StatelessWidget {
+ final String emoji;
+  Movable(this.emoji);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+        child: Container(
+          alignment: Alignment.center,
+          height: 50,
+          padding: EdgeInsets.all(5),
+          child: Text(emoji, style: TextStyle(color: Colors.black,fontSize: 40)) ,
+        ));
+     }
+}
---
+++ lib/emoji.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+class emoji {
+  int _num = 0;
+  final Map<String, String> choices = {
+    '🦁': ' حرف الالف',
+    '🐼': 'حرف الباء',
+    '🐪': 'حرف الجيم',
+    '🍎': 'حرف التاء',
+    '🐵': 'حرف القاف',
+    '🐯': 'حرف النون',
+    '🌳': 'حرف الشين',
+    '🤚': 'حرف الياء'
+  };
+ void next_Question()
+ {
+   if(_num < choices.length - 1)
+   {
+     _num ++ ;
+
+   }
+
+ }
+  Map<String, String> get_emoji()
+{
+  return choices;
+
+}
+bool is_finished()
+  {
+  if(_num >= choices.length-1)
+  {
+    return true;
+  }
+  else return false;
+
+  }
+}
